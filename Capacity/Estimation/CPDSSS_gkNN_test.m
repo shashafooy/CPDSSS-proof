b0649f8471c5854ac0d55433e845d28f7a61ae90
Aaron Pettit <apettit@pop-os.localdomain>
2023-12-17 12:47:44 -0700

    Added uniformization process for MI

--- Capacity/Estimation/CPDSSS_gkNN_test.m
+++ Capacity/Estimation/CPDSSS_gkNN_test.m
@@ -1,8 +1,8 @@
-num_samples=5e4;
-k=200;
+num_samples=1e3;
+k=30;
 % co.NSmethod='kdtree';
 
-N=2;L=2;T=10;
+N=2;L=1;T=10;
 M=N/L;P=N-N/L;
 
 
@@ -47,37 +47,53 @@ g_term=C2R_vector(g1_vector);
 H_xxc=HShannon_gkNN_estimation(xT_term,k);
 H_joint=HShannon_gkNN_estimation([xT_term;g_term],k);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% TOTAL X ENTROPY TEST%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+MI(1,1)=0;
+
+
+
 for k_scale = [1]
-for t=2:T
-% MI(X,Y|Z)
-k=100;
-% .k=40;
+    for t=2:T
+        % MI(X,Y|Z)
+        k=100;
+        % .k=40;
 
-xT_term=C2R_vector(squeeze(x_vectors(:,t,:)));
-xcond_term=C2R_vector(reshape(x_vectors(:,1:t-1,:),N*(t-1),num_samples));
-% xT_term=squeeze(x_vectors(:,t,:));
-% g_term=g1_vector;
-% xcond_term=reshape(x_vectors(:,1:t-1,:),N*(t-1),num_samples);
+        xT_term=C2R_vector(squeeze(x_vectors(:,t,:)));
+        xcond_term=C2R_vector(reshape(x_vectors(:,1:t-1,:),N*(t-1),num_samples));
+        % xT_term=squeeze(x_vectors(:,t,:));
+        % g_term=g1_vector;
+        % xcond_term=reshape(x_vectors(:,1:t-1,:),N*(t-1),num_samples);
 
-%Reuse previous iteration calculations
-H_gxc = H_joint;
-H_cond = H_xxc;
+        %Reuse previous iteration calculations
+        H_gxc = H_joint;
+        H_cond = H_xxc;
 
-% Cross terms betwen X,Z and Y,Z
-H_xxc = HShannon_gkNN_estimation([xT_term;xcond_term],k);
+        % Cross terms betwen X,Z and Y,Z
+        H_xxc = HShannon_gkNN_estimation([xT_term;xcond_term],k);
+        H_gknn(t)=H_xxc;
+        % co=HShannon_kNN_k_initialization(1);
+        % H_knn(t)=HShannon_kNN_k_estimation([xT_term;xcond_term],co);
 
-% H_gxc = HShannon_gkNN_estimation([g_term;xcond_term],k);
+        % H_gxc = HShannon_gkNN_estimation([g_term;xcond_term],k);
 
-% Joint entropy
-H_joint = HShannon_gkNN_estimation([xT_term;g_term;xcond_term],k);
+        % Joint entropy
+        H_joint = HShannon_gkNN_estimation([xT_term;g_term;xcond_term],k);
 
-% conditional entropy value
-% H_cond = HShannon_gkNN_estimation(xcond_term,k);
+        % conditional entropy value
+        % H_cond = HShannon_gkNN_estimation(xcond_term,k);
 
-MI(t,k_scale)=H_gxc + H_xxc - H_joint - H_cond;
+        MI(t,k_scale)=H_gxc + H_xxc - H_joint - H_cond;
+        % MI(t,k_scale)=H_xxc;
 
+    end
 end
-end
+figure(1)
+plot([1:10]*2,H_gknn,'-r*'),hold on
+plot([1:10]*2,H_knn,'-b*'),hold off
+legend("geometric knn","knn")
+
 figure(1)
 plot(1:T,MI)
 title("Individual conditional MI")
@@ -104,46 +120,46 @@ ylabel("I(g,x_T)"),shg
 
 
 function [X_R] = C2R_vector(X_C)
-    if(isreal(X_C))
-        X_R=X_C;
-        return
-    end
-    [N,sam]=size(X_C);
-    X_R=zeros(2*N,sam);
-    X_R(1:2:end)=real(X_C);
-    X_R(2:2:end)=imag(X_C);
+if(isreal(X_C))
+    X_R=X_C;
+    return
+end
+[N,sam]=size(X_C);
+X_R=zeros(2*N,sam);
+X_R(1:2:end)=real(X_C);
+X_R(2:2:end)=imag(X_C);
 end
 
 
 
 function [G,Q] = generate_G_Q(N,L)
-    epsilon=1e-4;
+epsilon=1e-4;
 
-    h=(randn(N,1)+1i*randn(N,1)).*exp(-[0:N-1]'/3);
-    h=randn(N,1).*exp(-[0:N-1]'/3); % REAL EXPERIMENT
-    H=toeplitz(h,[h(1); h(end:-1:2)]);
-    E=eye(N/L);E=upsample(E,L);
+h=(randn(N,1)+1i*randn(N,1)).*exp(-[0:N-1]'/3);
+h=randn(N,1).*exp(-[0:N-1]'/3); % REAL EXPERIMENT
+H=toeplitz(h,[h(1); h(end:-1:2)]);
+E=eye(N/L);E=upsample(E,L);
 
-    A=E'*H;
-    R=A'*A+epsilon*eye(N);
-    a=[1; zeros(N/L-1,1)];
-    p=A'*a;
-    g=R\p; 
+A=E'*H;
+R=A'*A+epsilon*eye(N);
+a=[1; zeros(N/L-1,1)];
+p=A'*a;
+g=R\p;
 
-    G=toeplitz(g,[g(1); g(end:-1:2)]);
-    G=G*E;
+G=toeplitz(g,[g(1); g(end:-1:2)]);
+G=G*E;
 
-    [V D]=eig(R);
-    NNL=N-N/L;
-    P=NNL;
-    Q=V(:,1:NNL);
-    % Q=Q';       %take hermitian to match X=SG + VQ
-      % v=(randn(T,P)+1i*randn(T,P))/sqrt(2);
+[V D]=eig(R);
+NNL=N-N/L;
+P=NNL;
+Q=V(:,1:NNL);
+% Q=Q';       %take hermitian to match X=SG + VQ
+% v=(randn(T,P)+1i*randn(T,P))/sqrt(2);
 
 
-    M=N/L;P=N-M;
-    G=randn(N,M);
-    Q=randn(N,P);
+M=N/L;P=N-M;
+G=randn(N,M);
+Q=randn(N,P);
 end
 
 
---
+++ Capacity/Estimation/Uniformization/NFEE-main/CPDSSS.py
@@ -0,0 +1,139 @@
+import numpy as np
+from scipy import stats
+
+from ent_est.entropy import kl, ksg, tkl, tksg
+from simulators.complex import mvn
+
+
+def UM_KL_Gaussian(x):
+    std_x=np.std(x,axis=0)
+    z=stats.norm.cdf(x,scale=std_x)
+    return tkl(z) - np.mean(np.log(np.prod(stats.norm.pdf(x,scale=std_x),axis=1)))
+
+N=2
+L=2
+M=int(N/L)
+P=N-int(N/L)
+
+
+
+T_range = range(N,7)
+n_trials = 100
+n_samples = 5000
+
+
+
+MI_tKL = np.empty(len(T_range))
+MI_means = np.empty(len(T_range))
+
+
+MI_cum = np.empty((n_trials,len(T_range)))
+H_gxc_cum=np.empty((n_trials,len(T_range)))
+H_xxc_cum=np.empty((n_trials,len(T_range)))
+H_joint_cum=np.empty((n_trials,len(T_range)))
+H_cond_cum=np.empty((n_trials,len(T_range)))
+
+        
+
+
+sim_G = mvn(rho=0.0, dim_x=N*M)
+sim_Q = mvn(rho=0.0, dim_x=N*P)
+
+for i in range(n_trials):        
+
+    # MI_cum = np.empty(n_trials)
+    # sim_mdl = mvn(rho=0.0, dim_x=d)
+    
+    # sim_S = mvn(rho=0.0, dim_x=int(M*T))
+    # sim_V = mvn(rho=0.0, dim_x=int(P*T))
+    
+    
+    
+    # cal2 = np.empty(n_trials)
+    # cal3 = np.empty(n_trials)
+    # cal4 = np.empty(n_trials)
+
+
+            
+    for k, T in enumerate(T_range):
+        sim_S = mvn(rho=0.0, dim_x=int(M*T))
+        sim_V = mvn(rho=0.0, dim_x=int(P*T))
+
+        n_sims = n_samples
+
+        s = sim_S.sim(n_samples=n_sims).reshape((n_sims,M,T))
+        # s=np.reshape(s,[n_sims,M,T])
+        v = sim_V.sim(n_samples=n_sims).reshape((n_sims,P,T))
+        # v=np.reshape(v,[P,T,n_sims])
+        G = sim_G.sim(n_samples=n_sims).reshape((n_sims,N,M))
+        # G=np.reshape(G,[N,M,n_sims])
+        Q = sim_Q.sim(n_samples=n_sims).reshape((n_sims,N,P))
+        # Q=np.reshape(Q,[N,P,n_sims])
+
+        #dims = (samples,N,T), matrix multiplication over last 2 dimensions
+        # X=np.matmul(np.transpose(G,(2,0,1)),np.transpose(s,(2,0,1))) + np.matmul(np.transpose(Q,(2,0,1)),np.transpose(v,(2,0,1)))
+        X=np.matmul(G,s)+np.matmul(Q,v)
+
+        g_term = G[:,:,0]
+        xT_term = X[:,:,T-1]
+        xCond_term = X[:,:,0:T-1].reshape((n_sims,N*(T-1)),order='F') #order 'F' needed to make arrays stack instead of interlaced
+        # xT_term = s[:,:,T-1]-.reshape((n_sims,M*(T-1)),order='F')
+
+        if(T>N): #Use previous calculations
+            H_gxc = H_joint
+            H_cond = H_xxc
+        else:
+            H_gxc = UM_KL_Gaussian(np.concatenate((xCond_term,g_term),axis=1))
+            H_cond = UM_KL_Gaussian(xCond_term)
+
+
+        H_xxc = UM_KL_Gaussian(np.concatenate((xCond_term,xT_term),axis=1))
+        H_joint = UM_KL_Gaussian(np.concatenate((xCond_term,xT_term,g_term),axis=1))
+
+        H_gxc_cum[i,k]=H_gxc
+        H_xxc_cum[i,k]=H_xxc
+        H_joint_cum[i,k]=H_joint
+        H_cond_cum[i,k]=H_cond
+        MI_cum[i,k] = H_gxc + H_xxc - H_joint - H_cond
+        # z=stats.norm.cdf(xT_term)
+        # H_xxc = tkl(z) - np.mean(np.log(np.prod(stats.norm.pdf(xT_term),axis=1)))
+
+        
+        # mse1[n,k] = np.mean((cal1-true_val)**2)
+        # mse2[n,k] = np.mean((cal2-true_val)**2)
+        # mse3[n,k] = np.mean((cal3-true_val)**2)
+        # mse4[n,k] = np.mean((cal4-true_val)**2)
+
+MI_tKL=np.mean(MI_cum,axis=0)
+MI_means=np.mean(H_gxc_cum,axis=0) + np.mean(H_xxc_cum,axis=0) - np.mean(H_joint_cum,axis=0) - np.mean(H_cond_cum,axis=0)
+MI_cumsum = np.cumsum(MI_means)
+
+    
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+            
+fig, ax = plt.subplots(2,1)
+# ax.set_yscale("log")
+            
+# ax.plot([N*t for t in T_range], H_KL, marker='o', color='b', linestyle=':', label='H KL', mfc='none')
+# ax.plot(T_range, np.sqrt(mse4[0]), marker='o', color='b', linestyle='-', label='UM-tKSG', mfc='none')   
+dims=[N*t for t in T_range] 
+ax[0].plot(dims, MI_tKL, marker='x', color='r', linestyle=':', label='MI individual')
+ax[0].plot(dims, MI_means, marker='x', color='b', linestyle='-', label='MI means')
+# ax.plot(T_range, np.sqrt(mse2[0]), marker='x', color='r', linestyle='-', label='KSG')
+        
+ax[0].set_xlabel('dimension')
+ax[0].set_ylabel('Entropy')
+ax[0].legend()
+
+ax[1].plot(dims,MI_cumsum,marker='x',linestyle='-')
+ax[1].set_xlabel('dimension')
+ax[1].set_ylabel('Entropy')
+ax[1].set_title('Cumulative MI')
+
+
+plt.savefig('figs/MI_cond_CPDSSSS')
+        
+import util.io
+import os
+util.io.save((T_range, MI_cum,H_gxc_cum,H_xxc_cum,H_joint_cum,H_cond_cum), os.path.join('temp_data', 'gaussian_experiment')) 
