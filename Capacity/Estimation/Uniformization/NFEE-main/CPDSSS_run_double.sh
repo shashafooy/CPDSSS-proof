72bc28e2582572fed4c1fa7b8a566c6e10778e55
= <=>
2024-05-30 16:47:33 -0600

    fix bash script to terminate properly

--- Capacity/Estimation/Uniformization/NFEE-main/CPDSSS_run_double.sh
+++ Capacity/Estimation/Uniformization/NFEE-main/CPDSSS_run_double.sh
@@ -4,10 +4,21 @@ source /home/aaron/anaconda3/etc/profile.d/conda.sh
 
 conda activate unif
 python CPDSSS.py &
+pid1=$!
 
 sleep 20
 
 conda activate uniform2
 python CPDSSS.py &
+pid2=$!
 
-wait
+cleanup() {
+    echo "Terminating both scripts"
+    kill $pid1 $pid2
+    wait $pid1 $pid2
+    echo "both scripts terminated"
+}
+
+trap 'cleanup' SIGINT
+
+wait $pid1 $pid2
