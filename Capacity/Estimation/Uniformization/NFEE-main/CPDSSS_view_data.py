Fix KL,KSG for cases of dupliicate points resulting in Inf entropy. Add functions for viewing data

Capacity/Estimation/Uniformization/NFEE-main/CPDSSS_view_data.py
@@ -3,44 +3,9 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+from misc_CPDSSS import viewData
+
 plt.rcParams['text.usetex']=True
-
-
-def append_data(old_data, old_T_range, new_data, new_T_range):
-    """
-    Adjust both datasets to match their T_range before appending
-    """
-
-    #Insert min_diff columns of NaN at beginning of matrix to align
-    min_diff = min(old_T_range) - min(new_T_range)
-    if(min_diff < 0): #Pad new data     
-        new_data = np.insert(new_data,[0]*abs(min_diff),np.nan,axis=1)
-    elif(min_diff > 0): #Pad old data
-        old_data = np.insert(old_data,[0]*min_diff,np.nan,axis=1)
-
-    #Insert max_diff columns of NaN at end of matrix to align
-    max_diff = max(old_T_range) - max(new_T_range)
-    if(max_diff > 0): #Pad new data
-        new_data = np.insert(new_data,[new_data.shape[1]]*max_diff,np.nan,axis=1)
-    elif(max_diff < 0): #Pad old data
-        old_data = np.insert(old_data,[old_data.shape[1]]*abs(max_diff),np.nan,axis=1)
-    
-    #Update total data and range
-    tot_data = np.append(old_data,new_data,axis=0)
-    tot_T_range = range(min(old_T_range[0],new_T_range[0]), max(old_T_range[-1],new_T_range[-1])+1)
-
-    return tot_data, tot_T_range
-
-def remove_outlier(data, num_std):
-    num_outlier = 1
-    while num_outlier > 0:
-        std = np.nanstd(data,axis=0)
-        mean = np.nanmean(data,axis=0)
-        #check if any value outside of 3*std
-        idx = (data > mean +num_std*std) |  (data < mean - num_std*std)
-        num_outlier = np.count_nonzero(idx)
-        data[idx]=np.NaN
-    return data
 
 """
 Load and combine all datasets
@@ -84,21 +49,21 @@ for filename in os.listdir(filepath):
         old_range = T_range
 
 
-    MI_tot,_ = append_data(MI_tot,old_range,MI_cum[iter,:],T_range)
-    H_gxc_tot,_=append_data(H_gxc_tot,old_range,H_gxc_cum[iter,:],T_range)
-    H_xxc_tot,_=append_data(H_xxc_tot,old_range,H_xxc_cum[iter,:],T_range)
-    H_joint_tot,_=append_data(H_joint_tot,old_range,H_joint_cum[iter,:],T_range)
-    H_cond_tot,old_range=append_data(H_cond_tot,old_range,H_cond_cum[iter,:],T_range)
+    MI_tot,_ = viewData.append_data(MI_tot,old_range,MI_cum[iter,:],T_range)
+    H_gxc_tot,_= viewData.append_data(H_gxc_tot,old_range,H_gxc_cum[iter,:],T_range)
+    H_xxc_tot,_= viewData.append_data(H_xxc_tot,old_range,H_xxc_cum[iter,:],T_range)
+    H_joint_tot,_= viewData.append_data(H_joint_tot,old_range,H_joint_cum[iter,:],T_range)
+    H_cond_tot,old_range= viewData.append_data(H_cond_tot,old_range,H_cond_cum[iter,:],T_range)
 
 '''
 Remove any data that is outside of 3 standard deviations. These data points can be considered outliers.
 '''
 if REMOVE_OUTLIERS:
-    MI_tot = remove_outlier(data=MI_tot,num_std=3)
-    H_gxc_tot = remove_outlier(data=H_gxc_tot,num_std=3)
-    H_xxc_tot = remove_outlier(data=H_xxc_tot,num_std=3)
-    H_joint_tot = remove_outlier(data=H_joint_tot,num_std=3)
-    H_cond_tot = remove_outlier(data=H_cond_tot,num_std=3)
+    MI_tot = viewData.remove_outlier(data=MI_tot,num_std=3)
+    H_gxc_tot = viewData.remove_outlier(data=H_gxc_tot,num_std=3)
+    H_xxc_tot = viewData.remove_outlier(data=H_xxc_tot,num_std=3)
+    H_joint_tot = viewData.remove_outlier(data=H_joint_tot,num_std=3)
+    H_cond_tot = viewData.remove_outlier(data=H_cond_tot,num_std=3)
 
 
 MI_mean = np.nanmean(MI_tot,axis=0)

Capacity/Estimation/Uniformization/NFEE-main/Laplace_view_data.py
@@ -4,45 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from simulators.CPDSSS_models import Laplace
+from misc_CPDSSS import viewData
 
 plt.rcParams['text.usetex']=True
 
 
-def append_data(old_data, old_T_range, new_data, new_T_range):
-    """
-    Adjust both datasets to match their T_range before appending
-    """
 
-    #Insert min_diff columns of NaN at beginning of matrix to align
-    min_diff = min(old_T_range) - min(new_T_range)
-    if(min_diff < 0): #Pad new data     
-        new_data = np.insert(new_data,[0]*abs(min_diff),np.nan,axis=1)
-    elif(min_diff > 0): #Pad old data
-        old_data = np.insert(old_data,[0]*min_diff,np.nan,axis=1)
-
-    #Insert max_diff columns of NaN at end of matrix to align
-    max_diff = max(old_T_range) - max(new_T_range)
-    if(max_diff > 0): #Pad new data
-        new_data = np.insert(new_data,[new_data.shape[1]]*max_diff,np.nan,axis=1)
-    elif(max_diff < 0): #Pad old data
-        old_data = np.insert(old_data,[old_data.shape[1]]*abs(max_diff),np.nan,axis=1)
-    
-    #Update total data and range
-    tot_data = np.append(old_data,new_data,axis=0)
-    tot_T_range = range(min(old_T_range[0],new_T_range[0]), max(old_T_range[-1],new_T_range[-1])+1)
-
-    return tot_data, tot_T_range
-
-def remove_outlier(data, num_std):
-    num_outlier = 1
-    while num_outlier > 0:
-        std = np.nanstd(data,axis=0)
-        mean = np.nanmean(data,axis=0)
-        #check if any value outside of 3*std
-        idx = (data > mean +num_std*std) |  (data < mean - num_std*std)
-        num_outlier = np.count_nonzero(idx)
-        data[idx]=np.NaN
-    return data
 
 """
 Load and combine all datasets
@@ -63,55 +30,126 @@ L=2
 # for idx,filepath in enumerate(filepaths):
 for filename in os.listdir(filepath):
     filename=os.path.splitext(filename)[0] #remove extention
-    _N_range,_H_unif_laplace,_H_KL_laplace,_MSE_uniform,_MSE_KL,_iter = util.io.load(os.path.join(filepath, filename))
+    _N_range,_H_unif_KL,_H_unif_KSG,_H_KL,_H_KSG,_iter = util.io.load(os.path.join(filepath, filename))
     iter = range(0,_iter+1)
 
     # Initialize arrays
-    if 'H_unif_laplace' not in locals():
-        H_unif_laplace = np.empty((0,_H_unif_laplace.shape[1]))
-        H_KL_laplace = np.empty((0,_H_KL_laplace.shape[1]))        
+    if 'N_range' not in locals():
         N_range = _N_range
+        N_size = len(N_range)
+        H_unif_KL = np.empty((0,N_size))
+        H_unif_KSG = np.empty((0,N_size))
+        H_KL = np.empty((0,N_size))        
+        H_KSG = np.empty((0,N_size))        
+       
 
 
-    H_unif_laplace,_ = append_data(H_unif_laplace,N_range,_H_unif_laplace[iter,:],_N_range)
-    H_KL_laplace,N_range = append_data(H_KL_laplace,N_range,_H_KL_laplace[iter,:],_N_range)
+    H_unif_KL,_ = viewData.append_data(H_unif_KL,N_range,_H_unif_KL,_N_range)
+    H_unif_KSG,_ = viewData.append_data(H_unif_KSG,N_range,_H_unif_KSG,_N_range)
+    H_KL,_ = viewData.append_data(H_KL,N_range,_H_KL,_N_range)
+    H_KSG,N_range = viewData.append_data(H_KSG,N_range,_H_KSG,_N_range)
     
+viewData.clean_data(H_unif_KL)
+viewData.clean_data(H_unif_KSG)
+viewData.clean_data(H_KL)
+viewData.clean_data(H_KSG)
 
-H_unif_laplace[np.isinf]=np.nan
-H_KL_laplace[np.isinf]=np.nan
 # Remove any data that is outside of 3 standard deviations. These data points can be considered outliers.
 if REMOVE_OUTLIERS:
-    H_unif_laplace = remove_outlier(data=H_unif_laplace,num_std=3)
-    H_KL_laplace = remove_outlier(data=H_KL_laplace,num_std=3)
+    H_unif_KL = viewData.remove_outlier(H_unif_KL)
+    H_unif_KSG = viewData.remove_outlier(H_unif_KSG)
+    H_KL = viewData.remove_outlier(H_KL)
+    H_KSG = viewData.remove_outlier(H_KSG)
 
 
-H_unif_mean = np.nanmean(H_unif_laplace,axis=0)
-H_KL_mean = np.nanmean(H_KL_laplace,axis=0)
+H_unif_KL_mean = np.nanmean(H_unif_KL,axis=0)
+H_unif_KSG_mean = np.nanmean(H_unif_KSG,axis=0)
+H_KL_mean = np.nanmean(H_KL,axis=0)
+H_KSG_mean = np.nanmean(H_KSG,axis=0)
 
-MSE_uniform = np.empty(len(N_range))
-MSE_KL = np.empty(len(N_range))
+H_unif_KL_std = np.nanstd(H_unif_KL,axis=0)
+H_unif_KSG_std = np.nanstd(H_unif_KSG,axis=0)
+H_KL_std = np.nanstd(H_KL,axis=0)
+H_KSG_std = np.nanstd(H_KSG,axis=0)
+
 H_true = np.empty(len(N_range))
-
 for i,N in enumerate(N_range):
     H_true[i] = Laplace(mu=0,b=2,N=N).entropy()
 
-MSE_uniform = np.nanmean((H_unif_laplace - H_true)**2,axis=0)
-MSE_KL = np.nanmean((H_KL_laplace - H_true)**2,axis=0)
 
+
+
+MSE_unif_KL = np.nanmean((H_unif_KL - H_true)**2,axis=0)
+MSE_unif_KSG = np.nanmean((H_unif_KSG - H_true)**2,axis=0)
+MSE_KL = np.nanmean((H_KL - H_true)**2,axis=0)
+MSE_KSG = np.nanmean((H_KSG - H_true)**2,axis=0)
+
+RMSE_unif_KL = np.sqrt(MSE_unif_KL)
+RMSE_unif_KSG = np.sqrt(MSE_unif_KSG)
+RMSE_KL = np.sqrt(MSE_KL)
+RMSE_KSG = np.sqrt(MSE_KSG)
+
+
+# PLOTS
+
+#entropy
 plt.figure(0)
-plt.plot(N_range,H_true,'-o',N_range,H_unif_mean,'--^',N_range,H_KL_mean,'--x')
+plt.plot(N_range,H_true,'-',
+         N_range,H_unif_KL_mean,'--^',
+         N_range,H_unif_KSG_mean,'--v',
+         N_range,H_KL_mean,'--x',
+         N_range,H_KSG_mean,'--o')
 plt.yscale("log")
 plt.title("Entropy of laplace distribution")
-plt.legend(["True H(x)","Uniform H(x)","KL H(x)"])
+plt.legend(["True H(x)","Uniform KL H(x)","Uniform KSG H(x)","KL H(x)","KSG H(x)"])
 plt.xlabel("N dimensions")
 plt.ylabel("H(x)")
 
+#Absolute error
 plt.figure(1)
-plt.plot(N_range,np.abs(H_true - H_unif_mean),'--^',N_range,abs(H_true - H_KL_mean),'--x')
+plt.plot(N_range,np.abs(H_true - H_unif_KL_mean),'--^',
+         N_range,np.abs(H_true - H_unif_KSG_mean),'--v',
+         N_range,np.abs(H_true - H_KL_mean),'--x',
+         N_range,np.abs(H_true - H_KSG_mean),'--o')
 plt.yscale("log")
 plt.title("Entropy Error")
-plt.legend(["Uniform error","KL error"])
+plt.legend(["Uniform KL","Uniform KSG","KL","KSG"])
 plt.xlabel("N dimensions")
 plt.ylabel("log error")
+
+#MSE
+plt.figure(2)
+plt.plot(N_range,MSE_unif_KL,'--^',
+         N_range,MSE_unif_KSG,'--v',
+         N_range,MSE_KL,'--x',
+         N_range,MSE_KSG,'--o')
+plt.yscale("log")
+plt.title("Entropy MSE Error")
+plt.legend(["Uniform KL","Uniform KSG","KL","KSG"])
+plt.xlabel("N dimensions")
+plt.ylabel("log MSE")
+
+#RMSE
+plt.figure(3)
+plt.plot(N_range,RMSE_unif_KL,'--^',
+         N_range,RMSE_unif_KSG,'--v',
+         N_range,RMSE_KL,'--x',
+         N_range,RMSE_KSG,'--o')
+plt.yscale("log")
+plt.title("Entropy RMSE Error")
+plt.legend(["Uniform KL","Uniform KSG","KL","KSG"])
+plt.xlabel("N dimensions")
+plt.ylabel("log RMSE")
+
+plt.figure(4)
+plt.plot(N_range,H_unif_KL_std,'--^',
+         N_range,H_unif_KSG_std,'--v',
+         N_range,H_KL_std,'--x',
+         N_range,H_KSG_std,'--o')
+plt.yscale("log")
+plt.title("Entropy std")
+plt.legend(["Uniform KL","Uniform KSG","KL","KSG"])
+plt.xlabel("N dimensions")
+plt.ylabel("log std")
 
 plt.show()

Capacity/Estimation/Uniformization/NFEE-main/ent_est/entropy.py
@@ -36,7 +36,10 @@ def kl(y, n=None, k=1, shuffle=True, standardize=True, rng=np.random):
     # knn search
     nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='auto', metric='chebyshev').fit(y)
     dist, idx = nbrs.kneighbors(y)
-    
+    zeros_mask = dist[:,k]!=0
+    dist = dist[zeros_mask,:]
+    N=dist.shape[0]
+
     if standardize == True:
         hh = dim*np.log(2*dist[:,k])+np.sum(np.log(y_std))
     else:
@@ -70,12 +73,17 @@ def tkl(y, n=None, k=1, shuffle=True, rng=np.random):
     # knn search
     nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='auto', metric='chebyshev',n_jobs=n_jobs).fit(y)
     dist, idx = nbrs.kneighbors(y)
+
+    zeros_mask = dist[:,k]!=0
     
     r = dist[:,k]
     r = np.tile(r[:, np.newaxis], (1, dim))
     lb = (y-r >= 0)*(y-r) + (y-r < 0)*0
     ub = (y+r <= 1)*(y+r) + (y+r > 1)*1
-    hh = np.log(np.prod(ub-lb, axis=1))
+
+    zeta = (ub-lb)[zeros_mask] #remove zeros, duplicate points result in 0 distance
+    N=zeta.shape[0]
+    hh = np.log(np.prod(zeta, axis=1))
         
     h = -spl.digamma(k)+spl.digamma(N)+np.mean(hh)
     
@@ -151,17 +159,21 @@ def ksg(y, n=None, k=1, shuffle=True, standardize=True, rng=np.random):
     nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='auto', metric='chebyshev').fit(y)
     dist, idx = nbrs.kneighbors(y)
     
-    hh = np.empty(n)
-    
-    if standardize == True:
-        for j in range(n):
-            r = np.max(np.abs(y[j]-y[idx[j,1:k+1]]), axis=0)
-            hh[j] = np.log(np.prod(2*r*y_std))
+    # hh = np.empty(n)
+    epsilons=np.abs(y-y[idx[:,k]])
+    zeros_mask = ~np.any(epsilons==0,axis=1)
+    epsilons=epsilons[zeros_mask]
+    if standardize == True:        
+        hh=np.sum(np.log(2*epsilons*y_std),axis=1)
+        # for j in range(n):
+        #     r = np.max(np.abs(y[j]-y[idx[j,1:k+1]]), axis=0)
+        #     hh[j] = np.log(np.prod(2*r*y_std))
             
     else:
-        for j in range(n):
-            r = np.max(np.abs(y[j]-y[idx[j,1:k+1]]), axis=0)
-            hh[j] = np.log(np.prod(2*r))
+        hh=np.sum(np.log(2*epsilons),axis=1)
+        # for j in range(n):
+        #     r = np.max(np.abs(y[j]-y[idx[j,1:k+1]]), axis=0)
+        #     hh[j] = np.log(np.prod(2*r))
         
     h = -spl.digamma(k)+spl.digamma(N)+(dim-1)/k+np.mean(hh)
     
@@ -190,14 +202,23 @@ def tksg(y, n=None, k=1, shuffle=True, rng=np.random):
     nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='auto', metric='chebyshev').fit(y)
     dist, idx = nbrs.kneighbors(y)
     
-    hh = np.empty(n)
-    for j in range(n):
-        r = np.max(np.abs(y[j]-y[idx[j,1:k+1]]), axis=0)
-        lb = (y[j]-r >=0)*(y[j]-r) + (y[j]-r < 0)*0
-        ub = (y[j]+r <=1)*(y[j]+r) + (y[j]+r > 1)*1
-        hh[j] = np.log(np.prod(ub-lb))
+    epsilons = np.abs(y-y[idx[:,k]])
+    zeta = np.minimum(y+epsilons,1) - np.maximum(y-epsilons,0)
+    #remove zeros, invalid data. Zeros occur if points along a dimension are exactly the same
+    zeros_mask = ~np.any(zeta==0,axis=1)
+    zeta=zeta[zeros_mask]
+    hh2=np.sum(np.log(zeta),axis=1)
+    N=zeta.shape[0]
+
+    # hh = np.empty(n)
+    # for j in range(n):
+    #     # r=dist[n,k]
+    #     r = np.max(np.abs(y[j]-y[idx[j,1:k+1]]), axis=0)
+    #     lb = (y[j]-r >=0)*(y[j]-r) + (y[j]-r < 0)*0
+    #     ub = (y[j]+r <=1)*(y[j]+r) + (y[j]+r > 1)*1
+    #     hh[j] = np.sum(np.log(ub-lb))
         
-    h = -spl.digamma(k)+spl.digamma(N)+(dim-1)/k+np.mean(hh)
+    h = -spl.digamma(k)+spl.digamma(N)+(dim-1)/k+np.mean(hh2)
     
     return h
 
@@ -579,23 +600,28 @@ class UMestimator:
 
         if(u.shape[0]<0.01*self.samples.shape[0]):
             return -1,0,0,0
-        if method == 'umtkl': 
-            z = stats.norm.cdf(u)
-            if(z.shape[0]<0.01*self.samples.shape[0]):
-                #return fitting error if size of z is < 1% of original data
-                return -1,0,0,0
-            correction1 = - np.mean(np.log(np.prod(stats.norm.pdf(u), axis=1)))
-            h = tkl(z, k=k) + correction1
-            
+        z = stats.norm.cdf(u)
+        correction1 = - np.mean(np.log(np.prod(stats.norm.pdf(u), axis=1)))
+
+        h2=0
+        if method == 'umtkl':            
+            # if(z.shape[0]<0.01*self.samples.shape[0]):
+            #     #return fitting error if size of z is < 1% of original data
+            #     return -1,0,0,0            
+            h = tkl(z, k=k) + correction1            
         elif method == 'umtksg':
-            z = stats.norm.cdf(u)
-            correction1 = - np.mean(np.log(np.prod(stats.norm.pdf(u), axis=1)))
+            # z = stats.norm.cdf(u)
+            # correction1 = - np.mean(np.log(np.prod(stats.norm.pdf(u), axis=1)))
             h = tksg(z, k=k) + correction1
+        elif method == 'both':
+            # z = sta
+            h = tkl(z,k=k) + correction1
+            h2 = tksg(z,k=k) + correction1
             
         correction2 = -np.mean(self.model.logdet_jacobi_u(samples)[idx])
             
         # return h+correction2, correction1+correction2, kl(u)+correction2, ksg(u)+correction2
-        return h+correction2,0,0,0
+        return h+correction2,h2+correction2,0,0
     
     def ksg_ent(self, k=1, reuse_samples=True, method='kl'):
         

Capacity/Estimation/Uniformization/NFEE-main/laplace_test.py
@@ -5,7 +5,7 @@ import misc_CPDSSS.entropy_util as ent
 import simulators.CPDSSS_models as mod
 import util.io
 
-from ent_est.entropy import kl
+from ent_est.entropy import kl,ksg
 
 
 
@@ -16,11 +16,14 @@ from ent_est.entropy import kl
 knn_samples = 200000
 n_train_samples = 30000
 n_trials = 100
-N_range=range(10,20)
+N_range=range(1,11)
+method='umtksg'
+# method='both'
 
-
-H_laplace = np.empty((n_trials,len(N_range)))*np.nan
+H_unif_KL = np.empty((n_trials,len(N_range)))*np.nan
+H_unif_KSG = np.empty((n_trials,len(N_range)))*np.nan
 H_KL_laplace = np.empty((n_trials,len(N_range)))*np.nan
+H_KSG_laplace = np.empty((n_trials,len(N_range)))*np.nan
 
 iter=0
 MSE_uniform=np.inf
@@ -29,25 +32,36 @@ MSE_KL=np.inf
 path = 'temp_data/laplace_test'
 today=date.today().strftime("%b_%d")
 filename = ent.update_filename(path,'',knn_samples,today,iter,rename=False)
-util.io.save((N_range,H_laplace,H_KL_laplace,MSE_uniform,MSE_KL,iter),os.path.join(path,filename))
+util.io.save((N_range,H_unif_KL,H_KL_laplace,MSE_uniform,MSE_KL,iter),os.path.join(path,filename))
 
 
 for i in range(n_trials):
-    for N in N_range:    
+    for ni,N in enumerate(N_range):    
         sim_laplace = mod.Laplace(mu=0,b=2,N=N)
         true_H_laplace = sim_laplace.entropy()        
         laplace_base = sim_laplace.sim(n_samples=knn_samples)
         
 
         ent.print_border("Calculate H(x) laplace, N={}, iter: {}".format(N,i+1))
-        H_laplace[i,N-1] = ent.calc_entropy(sim_model = sim_laplace, n_samples = n_train_samples,base_samples=laplace_base,val_tol=0.01)    
-        H_KL_laplace[i,N-1] = kl(laplace_base)
-        MSE_uniform = 1/(i+1) * np.linalg.norm(H_laplace[:i+1,N-1] - true_H_laplace,2)**2
-        MSE_KL = 1/(i+1) * np.linalg.norm(H_KL_laplace[:i+1,N-1] - true_H_laplace,2)**2
-        print("laplace entropy MSE: {}\nlaplace KL entropy MSE: {}".format(MSE_uniform,MSE_KL))
+        if method == 'umtksg':
+            H_unif_KSG[i,ni] = ent.calc_entropy(sim_model = sim_laplace, n_samples = n_train_samples,base_samples=laplace_base,val_tol=0.01,method=method)        
+            H_KSG_laplace[i,ni] = ksg(laplace_base)
+        elif method == 'umtkl':
+            H_unif_KL[i,ni] = ent.calc_entropy(sim_model = sim_laplace, n_samples = n_train_samples,base_samples=laplace_base,val_tol=0.01,method=method)        
+            H_KL_laplace[i,ni] = kl(laplace_base)
+        else:
+            H_unif_KL[i,ni],H_unif_KSG[i,ni] = ent.calc_entropy(sim_model = sim_laplace, n_samples = n_train_samples,base_samples=laplace_base,val_tol=0.01,method=method)        
+            H_KL_laplace[i,ni] = kl(laplace_base)
+            H_KSG_laplace[i,ni] = ksg(laplace_base)
+
+        # H_unif_KL[i,ni],H_unif_KSG[i,ni] = ent.calc_entropy(sim_model = sim_laplace, n_samples = n_train_samples,base_samples=laplace_base,val_tol=0.01,method='both')           
+        
+        # MSE_uniform = np.mean((H_unif_KL[:i+1,N-1] - true_H_laplace)**2)
+        # MSE_KL = np.mean((H_KL_laplace[:i+1,N-1] - true_H_laplace)**2)
+        # print("laplace entropy MSE: {}\nlaplace KL entropy MSE: {}".format(MSE_uniform,MSE_KL))
 
         if N==N_range[-1]:
             iter=iter+1
             filename=ent.update_filename(path,filename,knn_samples,today,iter,rename=True)
-        util.io.save((N_range,H_laplace,H_KL_laplace,MSE_uniform,MSE_KL,iter),os.path.join(path,filename))
+        util.io.save((N_range,H_unif_KL,H_unif_KSG,H_KL_laplace,H_KSG_laplace,iter),os.path.join(path,filename))
 