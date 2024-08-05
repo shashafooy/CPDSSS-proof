3804de29b08dabc9dff4af240e9aeb16704772f9
Aaron Pettit <aaron.pettit@comcast.net>
2024-08-05 14:10:13 -0600

    update sim_model

--- Capacity/Estimation/Uniformization/NFEE-main/CPDSSS.py
+++ Capacity/Estimation/Uniformization/NFEE-main/CPDSSS.py
@@ -80,7 +80,7 @@ for i in range(n_trials):
     for k, T in enumerate(T_range):
         sim_model = CPDSSS(T,N,L,use_gaussian_approx=GQ_gaussian)
         #generate base samples based on max dimension
-        sim_model.use_chan_in_sim()
+        sim_model.set_dim_joint()
         knn_samples = int(min(min_knn_samples, 0.75*n_train_samples * sim_model.x_dim))
         X,X_T,X_cond,h = sim_model.get_base_X_h(knn_samples)
         hxc=np.concatenate((X_cond,h),axis=1)
@@ -128,8 +128,9 @@ for i in range(n_trials):
         
         #Train H(h,x_cond)
         misc.print_border("1/4 calculating H(h,x_old), T: {0}, iter: {1}".format(T,i+1))        
-        prev_tx_model.use_chan_in_sim()
-        H_hxc_thread,H_hxc_correction,estimator = ent.calc_entropy_thread(prev_tx_model,n_train_samples,hxc)
+        # prev_tx_model.use_chan_in_sim()
+        sim_model.set_dim_hxc()
+        H_hxc_thread,H_hxc_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,hxc)
         if save_best_model:
             best_trn_loss[T-1,0] = ent.update_best_model(estimator.model,hxc,best_trn_loss[T-1,0],f'CPDSSS_hxc_{T}T',model_path)
         if H_joint_thread is not None: #don't run if first iteration
@@ -145,8 +146,9 @@ for i in range(n_trials):
 
         #Train H(x_cond)
         misc.print_border("2/4 calculating H(x_old), T: {0}, iter: {1}".format(T,i+1))
-        prev_tx_model.use_chan_in_sim(False)
-        H_cond_thread,H_cond_correction,estimator = ent.calc_entropy_thread(prev_tx_model,n_train_samples,X_cond)        
+        # prev_tx_model.use_chan_in_sim(False)
+        sim_model.set_dim_cond()
+        H_cond_thread,H_cond_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,X_cond)        
         if save_best_model:
             best_trn_loss[T-1,1] = ent.update_best_model(estimator.model,X_cond,best_trn_loss[T-1,1],f'CPDSSS_Xcond_{T}T',model_path)
         #wait for knn H(h,x_cond)
@@ -157,7 +159,8 @@ for i in range(n_trials):
 
         #Train H(x_T,x_cond)
         misc.print_border("3/4 calculating H(x_T, x_old), T: {0}, iter: {1}".format(T,i+1))
-        sim_model.use_chan_in_sim(False)
+        # sim_model.use_chan_in_sim(False)
+        sim_model.set_dim_xxc()
         H_xxc_thread, H_xxc_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,X)
         if save_best_model:
             best_trn_loss[T-1,2] = ent.update_best_model(estimator.model,X,best_trn_loss[T-1,2],f'CPDSSS_xxc_{T}T',model_path)
@@ -169,7 +172,8 @@ for i in range(n_trials):
         
         #Train H(h,x_T,x_cond)
         misc.print_border("4/4 calculating H_(h,x_T,x_old), T: {0}, iter: {1}".format(T,i+1))
-        sim_model.use_chan_in_sim()
+        # sim_model.use_chan_in_sim()
+        sim_model.set_dim_joint()
         H_joint_thread, H_joint_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,joint)        
         if save_best_model:
             best_trn_loss[T-1,3] = ent.update_best_model(estimator.model,joint,best_trn_loss[T-1,3],f'CPDSSS_joint_{T}T',model_path)
