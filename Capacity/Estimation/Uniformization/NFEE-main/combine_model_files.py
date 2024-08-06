9b15a06503b3e9db29330ad8ce7fb73d1547f18f
= <=>
2024-08-06 10:32:24 -0600

    combine best models

--- Capacity/Estimation/Uniformization/NFEE-main/combine_model_files.py
+++ Capacity/Estimation/Uniformization/NFEE-main/combine_model_files.py
@@ -29,8 +29,8 @@ L=2
 M=int(N/L)
 P=N-int(N/L)
 
-min_samples = 20000 #samples to generate per entropy calc
-n_train_samples = 1000
+min_samples = 2000000 #samples to generate per entropy calc
+n_train_samples = 100000
 GQ_gaussian = False
 
 """
@@ -49,7 +49,7 @@ for folder in sub_folders:
     hpc_folder = os.path.join(model_path_new,folder)
     
     T = int(folder[:-1])
-    print(f"Comparing models for {T}T")
+    print(f"\nComparing models for {T}T")
     
     sim_model = CPDSSS(T,N,L,use_gaussian_approx=GQ_gaussian)
     #generate base samples based on max dimension
@@ -60,48 +60,25 @@ for folder in sub_folders:
     joint=np.concatenate((X,h),axis=1)
 
     #Compare with new model files
-    # sim_model.set_dim_hxc()
-    # compare_models(hxc,'hxc',base_folder,hpc_folder)
-    # name = f'CPDSSS_hxc_{folder}'
-    # compare_models(hxc,name,base_folder,name,hpc_folder)
+    name = f'CPDSSS_hxc_{folder}'
+    compare_models(hxc,name,base_folder,name,hpc_folder)
 
-    # sim_model.set_dim_xxc()
-    # name = f'CPDSSS_xxc_{folder}'
-    # compare_models(X,name,base_folder,name,hpc_folder)
+    name = f'CPDSSS_xxc_{folder}'
+    compare_models(X,name,base_folder,name,hpc_folder)
 
-    # sim_model.set_dim_cond()
-    # name = f'CPDSSS_Xcond_{folder}'
-    # compare_models(X_cond,name,base_folder,name,hpc_folder)
+    name = f'CPDSSS_Xcond_{folder}'
+    compare_models(X_cond,name,base_folder,name,hpc_folder)
 
-    # sim_model.set_dim_joint()
-    # name = f'CPDSSS_joint_{folder}'
-    # compare_models(joint,name,base_folder,name,hpc_folder)
+    name = f'CPDSSS_joint_{folder}'
+    compare_models(joint,name,base_folder,name,hpc_folder)
 
     #Compare with models from previous T, but same distribution
     if folder != sub_folders[0]:
-        print(f"Comparing models {T}T with previous {T-1}T")
+        print(f"\nComparing models {T}T with previous {T-1}T")
         sim_model.set_dim_hxc()
         compare_models(hxc,f'CPDSSS_hxc_{T}T',base_folder,f'CPDSSS_joint_{T-1}T',prev_folder)
 
         sim_model.set_dim_cond()
         compare_models(X_cond,f'CPDSSS_Xcond_{T}T',base_folder,f'CPDSSS_xxc_{T-1}T',prev_folder)
-    
 
     prev_folder = base_folder
-
-
-
-#Compare with models with different T, but same distribution
-for i,folder in enumerate(sub_folders[1:]):
-    base_folder = os.path.join(model_path_old,folder)
-    prev_folder = os.path.join(model_path_old,sub_folders[i])
-
-    T = int(folder[:-1])
-    
-    sim_model = CPDSSS(T,N,L,use_gaussian_approx=GQ_gaussian)
-    #generate base samples based on max dimension
-    sim_model.set_dim_joint()
-    knn_samples = int(min(min_samples, 0.75*n_train_samples * sim_model.x_dim))
-    X,X_T,X_cond,h = sim_model.get_base_X_h(knn_samples)
-    hxc=np.concatenate((X_cond,h),axis=1)
-    joint=np.concatenate((X,h),axis=1)
