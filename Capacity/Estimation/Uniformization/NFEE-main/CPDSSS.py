d72bc1f57c0b844c1e1b8ff17f434e3fb5f2c4fb
= <=>
2024-08-18 15:22:41 -0600

    modularize CPDSSS

--- Capacity/Estimation/Uniformization/NFEE-main/CPDSSS.py
+++ Capacity/Estimation/Uniformization/NFEE-main/CPDSSS.py
@@ -14,25 +14,52 @@ config = configparser.ConfigParser()
 config.read('CPDSSS.ini')
 KNN_THREADING = not config['GLOBAL'].getboolean('knn_GPU',False) #Use threading if GPU not used
 
+SAVE_MODEL = True
+TRAIN_ONLY = True
+REUSE_MODEL = False
+
+def run_CPDSSS(sim_model,base_samples,old_thread = None,old_correction = 0,model_name = '', model_path = ''):
+    
+    model = ent.load_model(name=model_name,path = model_path) if REUSE_MODEL else None
+    if TRAIN_ONLY:
+        estimator = ent.learn_model(sim_model,
+                    pretrained_model=model,
+                    train_samples= base_samples)
+        H=0
+    else:
+        if KNN_THREADING:            
+            new_thread,new_correction,estimator = ent.calc_entropy_thread(sim_model,base_samples = base_samples,model = model)
+            if old_thread is not None: #don't run if first iteration            
+                knn = old_thread.get_result()
+                H = knn + old_correction         
+        else:
+            H,estimator = ent.calc_entropy(sim_model,base_samples=base_samples,model = model)
+    if SAVE_MODEL:
+        _ = ent.update_best_model(estimator.model,base_samples,name=model_name,path=model_path)
+
+    if KNN_THREADING:
+        return H,new_thread,new_correction
+    else:
+        return H
+
 
 
 """
 Parameters for CPDSSS
 """
-N=2
+N=6
 L=2
 M=int(N/L)
 P=N-int(N/L)
 T_range = range(2,8)
 
-SAVE_MODEL = True
-REUSE_MODEL = True
+
 
 """
 Number of iterations
 """
 n_trials = 100 #iterations to average
-min_knn_samples = 1000000 #samples to generate per entropy calc
+min_knn_samples = 2000000 #samples to generate per entropy calc
 n_train_samples = 100000
 
 
@@ -76,7 +103,8 @@ XH_path = os.path.join(model_path,'XH')
 
 
 #fix filename if file already exists
-filename = misc.update_filename(path,filename,-1,rename=False)     
+if not TRAIN_ONLY:
+    filename = misc.update_filename(path,filename,-1,rename=False)     
 # model = ent.load_model(8,'CPDSSS_hxc_2T','temp_data/saved_models/2T')
 
 
@@ -104,91 +132,67 @@ for i in range(n_trials):
                 combine knn entropy with jacobian correction term (main thread)
                 Start new model while current knn is running (main thread)
          """
-        
+                                
         '''Train H(h,x_cond)'''
         misc.print_border("1/4 calculating H(h,x_old), T: {0}, iter: {1}".format(T,i+1))        
         sim_model.set_dim_hxc()
-        name = f'{T-1}T'        
-        if REUSE_MODEL:
-            model = ent.load_model(name=name,path = XH_path)
-        if KNN_THREADING:            
-            H_hxc_thread,H_hxc_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,hxc,model = model)
-            if H_joint_thread is not None: #don't run if first iteration            
-                knn = H_joint_thread.get_result()
-                H_joint[prev_idx] = knn + H_joint_correction         
-                MI[prev_idx] = H_hxc[prev_idx] + H_xxc[prev_idx] - H_joint[prev_idx] - H_cond[prev_idx]
+        name = f'{T-1}T'   
+
+        if KNN_THREADING:
+            H_joint[prev_idx],H_hxc_thread,H_hxc_correction = run_CPDSSS(sim_model,hxc,H_joint_thread,H_joint_correction,name,XH_path)
+            MI[prev_idx] = H_hxc[prev_idx] + H_xxc[prev_idx] - H_joint[prev_idx] - H_cond[prev_idx]
         else:
-            H_hxc[index],estimator = ent.calc_entropy(sim_model,n_train_samples,hxc,model = model)
-        if SAVE_MODEL:
-            best_trn_loss[T-1,0] = ent.update_best_model(estimator.model,hxc,best_trn_loss[T-1,0],name,XH_path)
-        
-        filename = misc.update_filename(path,filename,i)            
-        util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
+            H_hxc[index]=run_CPDSSS(sim_model,hxc,model_name=name,model_path=XH_path)
+
+        if not TRAIN_ONLY:
+            filename = misc.update_filename(path,filename,i)            
+            util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
 
         '''Train H(x_cond)'''
         misc.print_border("2/4 calculating H(x_old), T: {0}, iter: {1}".format(T,i+1))
         
         sim_model.set_dim_cond()
         name=f'{T-1}T'
-        if REUSE_MODEL:
-            model = ent.load_model(name=name,path = X_path)
         if KNN_THREADING:
-            H_cond_thread,H_cond_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,X_cond,model=model)        
-            #wait for knn H(h,x_cond)
-            knn = H_hxc_thread.get_result()        
-            H_hxc[index] = knn + H_hxc_correction #knn + correction
+            H_hxc[index],H_cond_thread,H_cond_correction = run_CPDSSS(sim_model,X_cond,H_hxc_thread,H_hxc_correction,name,X_path)
         else:
-            H_cond[index],estimator = ent.calc_entropy(sim_model,n_train_samples,X_cond,model=model)        
-        if SAVE_MODEL:
-            best_trn_loss[T-1,1] = ent.update_best_model(estimator.model,X_cond,best_trn_loss[T-1,1],name,X_path)
-        
-        
-        util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
+            H_cond[index]=run_CPDSSS(sim_model,X_cond,model_name=name,model_path=X_path)
+
+        if not TRAIN_ONLY:
+            util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
 
         '''Train H(x_T,x_cond)'''
         misc.print_border("3/4 calculating H(x_T, x_old), T: {0}, iter: {1}".format(T,i+1))        
         sim_model.set_dim_xxc()
         name=f'{T}T'
-        if REUSE_MODEL:
-            model = ent.load_model(name=name,path = X_path)
+
         if KNN_THREADING:
-            H_xxc_thread, H_xxc_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,X,model=model)
-            knn = H_cond_thread.get_result() #previous result
-            H_cond[index] = knn + H_cond_correction
+            H_cond[index],H_xxc_thread,H_xxc_correction = run_CPDSSS(sim_model,X,H_cond_thread,H_cond_correction,name,X_path)
         else:
-            H_xxc[index], estimator = ent.calc_entropy(sim_model,n_train_samples,X,model=model)
-        if SAVE_MODEL:
-            best_trn_loss[T-1,2] = ent.update_best_model(estimator.model,X,best_trn_loss[T-1,2],name,X_path)
-                
-        util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
+            H_xxc[index]=run_CPDSSS(sim_model,X,model_name=name,model_path=X_path)        
+        if not TRAIN_ONLY:    
+            util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
         
         '''Train H(h,x_T,x_cond)'''
         misc.print_border("4/4 calculating H_(h,x_T,x_old), T: {0}, iter: {1}".format(T,i+1))
         sim_model.set_dim_joint()
         name=f'{T}T'
-        if REUSE_MODEL:
-            model = ent.load_model(name=name,path = XH_path)
-        if KNN_THREADING:
-            H_joint_thread, H_joint_correction,estimator = ent.calc_entropy_thread(sim_model,n_train_samples,joint,model=model)        
-            knn = H_xxc_thread.get_result()
-            H_xxc[index] = knn + H_xxc_correction
-        else:
-            H_joint[index], estimator = ent.calc_entropy(sim_model,n_train_samples,joint,model=model)        
-            MI[index] = H_hxc[index] + H_xxc[index] - H_joint[index] - H_cond[index]
-        if SAVE_MODEL:
-            best_trn_loss[T-1,3] = ent.update_best_model(estimator.model,joint,best_trn_loss[T-1,3],name,XH_path)
-        #wait for knn H(x_T,x_cond)
-        
 
-        util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
+        if KNN_THREADING:
+            H_xxc[index],H_joint_thread,H_joint_correction = run_CPDSSS(sim_model,joint,H_xxc_thread,H_xxc_correction,name,XH_path)
+        else:
+            H_joint[index]=run_CPDSSS(sim_model,joint,model_name=name,model_path=XH_path)
+            MI[index] = H_hxc[index] + H_xxc[index] - H_joint[index] - H_cond[index]
+
+        if not TRAIN_ONLY:
+            util.io.save((T_range, MI,H_hxc,H_xxc,H_joint,H_cond,i), os.path.join(path,filename)) 
 
         #Save this index set for next iteration
         prev_idx = index
 
 
-        # if k== np.size(T_range)-1:
-        #     completed_iter = completed_iter + 1
-    filename = misc.update_filename(path,filename,i+1)            
+    if not TRAIN_ONLY:
+        filename = misc.update_filename(path,filename,i+1)            
    
 
 #get knn H(joint) from final iteraiton
