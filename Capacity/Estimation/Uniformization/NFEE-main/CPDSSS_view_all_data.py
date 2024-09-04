1acc2a816bba6a097e1076eca28da18a80130afe
Aaron Pettit <aaron.pettit@comcast.net>
2024-09-04 15:16:33 -0600

    Add script for viewing all N data for CPDSSS together. Update training epochs to reflected updated minibatch on fine tuning. Update laplace_test for a rerun to view performance of system

---
+++ Capacity/Estimation/Uniformization/NFEE-main/CPDSSS_view_all_data.py
@@ -0,0 +1,170 @@
+import util.io
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+from misc_CPDSSS import viewData
+
+plt.rcParams["text.usetex"] = True
+
+"""
+Load and combine all datasets
+"""
+max_T = 0
+min_T = 0
+
+N_range = [2, 4, 6]
+L = 2
+
+REMOVE_OUTLIERS = True
+COMBINE_ENTROPIES = True
+
+
+MI_mean = []
+H_hxc_mean = []
+H_xxc_mean = []
+H_joint_mean = []
+H_cond_mean = []
+
+MI_std = []
+H_hxc_std = []
+H_xxc_std = []
+H_joint_std = []
+H_cond_std = []
+
+H_h = []
+
+for i, N in enumerate(N_range):
+
+    # util.io.save((T_range, MI_cum,H_gxc_cum,H_xxc_cum,H_joint_cum,H_cond_cum,completed_iter), os.path.join(filepath,filename))
+    base_path = f"temp_data/CPDSSS_data/MI(h,X)/N{N}_L2/"
+    filepath = base_path + "pretrained_model"
+
+    # filepath=filepaths[1]
+    # idx=0
+    # for idx,filepath in enumerate(filepaths):
+    for filename in os.listdir(filepath):
+        filename = os.path.splitext(filename)[0]  # remove extention
+        T_range, MI_cum, H_gxc_cum, H_xxc_cum, H_joint_cum, H_cond_cum, completed_iter = (
+            util.io.load(os.path.join(filepath, filename))
+        )
+        iter = range(0, completed_iter + 1)
+
+        if "MI_tot" not in locals():
+            MI_tot = np.empty((0, np.size(T_range)))
+            H_gxc_tot = np.empty((0, np.size(T_range)))
+            H_xxc_tot = np.empty((0, np.size(T_range)))
+            H_joint_tot = np.empty((0, np.size(T_range)))
+            H_cond_tot = np.empty((0, np.size(T_range)))
+            old_range = T_range
+
+        MI_tot, _ = viewData.align_and_concatenate(MI_tot, MI_cum, old_range, T_range)
+        H_gxc_tot, _ = viewData.align_and_concatenate(H_gxc_tot, H_gxc_cum, old_range, T_range)
+        H_xxc_tot, _ = viewData.align_and_concatenate(H_xxc_tot, H_xxc_cum, old_range, T_range)
+        H_joint_tot, _ = viewData.align_and_concatenate(
+            H_joint_tot, H_joint_cum, old_range, T_range
+        )
+        H_cond_tot, old_range = viewData.align_and_concatenate(
+            H_cond_tot, H_cond_cum, old_range, T_range
+        )
+
+    T_range = old_range
+    """
+    Remove any data that is outside of 3 standard deviations. These data points can be considered outliers.
+    """
+    if REMOVE_OUTLIERS:
+        MI_tot = viewData.remove_outlier(data=MI_tot, num_std=3)
+        H_gxc_tot = viewData.remove_outlier(data=H_gxc_tot, num_std=3)
+        H_xxc_tot = viewData.remove_outlier(data=H_xxc_tot, num_std=3)
+        H_joint_tot = viewData.remove_outlier(data=H_joint_tot, num_std=3)
+        H_cond_tot = viewData.remove_outlier(data=H_cond_tot, num_std=3)
+
+    MI_mean.append(np.nanmean(MI_tot, axis=0))
+    H_hxc_mean.append(np.nanmean(H_gxc_tot, axis=0))
+    H_xxc_mean.append(np.nanmean(H_xxc_tot, axis=0))
+    H_joint_mean.append(np.nanmean(H_joint_tot, axis=0))
+    H_cond_mean.append(np.nanmean(H_cond_tot, axis=0))
+
+    MI_std.append(np.nanstd(MI_tot, axis=0))
+    H_hxc_std.append(np.nanstd(H_gxc_tot, axis=0))
+    H_xxc_std.append(np.nanstd(H_xxc_tot, axis=0))
+    H_joint_std.append(np.nanstd(H_joint_tot, axis=0))
+    H_cond_std.append(np.nanstd(H_cond_tot, axis=0))
+
+    T_range = old_range
+    # Potentially more accurate taking into account each mean value
+    MI_mean_sum = H_hxc_mean[-1] + H_xxc_mean[-1] - H_joint_mean[-1] - H_cond_mean[-1]
+
+    del MI_tot
+
+    """
+    Max capacity
+    """
+    from simulators.CPDSSS_models import CPDSSS
+
+    sim_model = CPDSSS(1, N, L, False)
+    H_h.append(sim_model.chan_entropy())
+
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
+# fig4, ax4 = plt.subplots(2, 1)
+
+for i, N in enumerate(N_range):
+    """Plot individual and cumulative Mutual Information"""
+
+    _MI_mean = MI_mean[i]
+    # fig1.suptitle("N={}, L={}".format(N, L))
+    temp_range = range(1, max(T_range) + 1)
+    temp_range = np.insert(T_range, 0, 1)
+    _MI_mean = np.insert(_MI_mean, 0, 0)  # start at 0 MI
+    ax1.plot(temp_range, _MI_mean, label=rf"$N={N}$")
+    ax1.set_title(r"Individual $I(\mathbf{h},\mathbf{x}_T | \mathbf{x}_{1:T-1})$")
+    ax1.set_xlabel(r"$T$ Transmissions")
+    ax1.set_ylabel(r"Entropy")
+
+    (line,) = ax2.plot(temp_range, np.cumsum(_MI_mean), label=rf"$N={N}$")
+    ax2.axhline(y=H_h[i], linestyle="dashed", color=line.get_color())
+    ax2.set_title(r"Total $I(\mathbf{h},\mathbf{X})$")
+    ax2.set_xlabel(r"$T$ Transmissions")
+    ax2.set_ylabel(r"Entropy")
+    ax2.text(
+        x=1,
+        y=H_h[i] + 0.02,
+        s=rf"$H(\mathbf{{h}})$",
+        fontsize=10,
+        verticalalignment="bottom",
+        horizontalalignment="left",
+    )
+
+    # """Plot Mutual Information, but include bars showing variance"""
+
+    # # fig4.suptitle("N={}, L={}".format(N, L))
+    # yerr = np.insert(MI_std[i], 0, 0)
+    # ax4[0].errorbar(temp_range, _MI_mean, yerr=yerr, label=rf"$N={N}$")
+    # ax4[0].set_title(r"Individual $I(\mathbf{h},\mathbf{x}_T | \mathbf{x}_{1:T-1})$")
+    # ax4[0].set_xlabel(r"$T$")
+    # line, caps, bars = ax4[1].errorbar(
+    #     temp_range, np.cumsum(_MI_mean), yerr=np.cumsum(yerr), label=rf"$N={N}$"
+    # )
+    # ax4[1].set_title(r"Total $I(\mathbf{h},\mathbf{X})$")
+    # ax4[1].set_xlabel(r"T")
+    # ax4[1].axhline(
+    #     y=H_h[i], linestyle="dashed", color=line.get_color()
+    # )  # , label=rf"$H(\mathbf{{h}}),N={N}$")
+    # # Add text to the dashed line
+    # ax4[1].text(
+    #     x=1,
+    #     y=H_h[i] + 0.02,
+    #     s=rf"$H(\mathbf{{h}})$",
+    #     fontsize=10,
+    #     verticalalignment="bottom",
+    #     horizontalalignment="left",
+    # )
+
+
+ax1.legend(loc="upper right")
+ax2.legend(loc="upper right")
+
+fig1.tight_layout()
+fig2.tight_layout()
+plt.show()
--- Capacity/Estimation/Uniformization/NFEE-main/laplace_test.py
+++ Capacity/Estimation/Uniformization/NFEE-main/laplace_test.py
@@ -6,13 +6,15 @@ import misc_CPDSSS.util as misc
 import simulators.CPDSSS_models as mod
 import util.io
 
-from ent_est.entropy import kl, ksg
+from ent_est.entropy import kl_ksg
+
+TRAIN_ONLY = True
 
 
-knn_samples = 200000
+# knn_samples = 200000
 n_train_samples = 100000
 n_trials = 100
-N_range = range(11, 21)
+N_range = range(1, 20)
 method = "both"
 patience = 5
 val_tol = 0.1
@@ -33,55 +35,57 @@ filename = "laplace_data({})".format(today)
 filename = misc.update_filename(path=path, old_name=filename, iter=iter, rename=False)
 # util.io.save((N_range,H_unif_KL,H_KL_laplace,MSE_uniform,MSE_KL,iter),os.path.join(path,filename))
 
+model_path = "temp_data/saved_models/laplace"
+
 
 for i in range(n_trials):
     for ni, N in enumerate(N_range):
+        model_name = f"{N}N"
+
+        knn_samples = n_train_samples * N
         sim_laplace = mod.Laplace(mu=0, b=2, N=N)
         true_H_laplace = sim_laplace.entropy()
         laplace_base = sim_laplace.sim(n_samples=knn_samples)
 
-        misc.print_border("Calculate H(x) laplace, N={}, iter: {}".format(N, i + 1))
-        if method == "umtksg":
-            H_unif_KSG[i, ni] = ent.calc_entropy(
-                sim_model=sim_laplace,
-                n_samples=n_train_samples,
-                base_samples=laplace_base,
-                val_tol=val_tol,
-                patience=patience,
-                method=method,
+        if TRAIN_ONLY:
+            estimator = ent.learn_model(sim_model=sim_laplace, train_samples=laplace_base)
+            _ = ent.update_best_model(
+                estimator.model, laplace_base, name=model_name, path=model_path
             )
-            H_KSG_laplace[i, ni] = ksg(laplace_base)
-        elif method == "umtkl":
-            H_unif_KL[i, ni] = ent.calc_entropy(
-                sim_model=sim_laplace,
-                n_samples=n_train_samples,
-                base_samples=laplace_base,
-                val_tol=0.01,
-                patience=3,
-                method=method,
-            )
-            H_KL_laplace[i, ni] = kl(laplace_base)
         else:
-            H_unif_KL[i, ni], H_unif_KSG[i, ni] = ent.calc_entropy(
-                sim_model=sim_laplace,
-                n_samples=n_train_samples,
-                base_samples=laplace_base,
-                val_tol=0.01,
-                method=method,
+            misc.print_border("Calculate H(x) laplace, N={}, iter: {}".format(N, i + 1))
+            if method == "umtksg":
+                H_unif_KSG[i, ni] = ent.calc_entropy(
+                    sim_model=sim_laplace,
+                    base_samples=laplace_base,
+                    method=method,
+                )
+                H_KSG_laplace[i, ni] = ksg(laplace_base)
+            elif method == "umtkl":
+                H_unif_KL[i, ni] = ent.calc_entropy(
+                    sim_model=sim_laplace,
+                    base_samples=laplace_base,
+                    method=method,
+                )
+                H_KL_laplace[i, ni] = kl(laplace_base)
+            else:
+                H_unif_KL[i, ni], H_unif_KSG[i, ni] = ent.calc_entropy(
+                    sim_model=sim_laplace,
+                    base_samples=laplace_base,
+                    method=method,
+                )
+                H_KL_laplace[i, ni], H_KSG_laplace[i, ni] = kl_ksg(laplace_base)
+
+            # H_unif_KL[i,ni],H_unif_KSG[i,ni] = ent.calc_entropy(sim_model = sim_laplace, n_samples = n_train_samples,base_samples=laplace_base,val_tol=0.01,method='both')
+
+            # MSE_uniform = np.mean((H_unif_KL[:i+1,N-1] - true_H_laplace)**2)
+            # MSE_KL = np.mean((H_KL_laplace[:i+1,N-1] - true_H_laplace)**2)
+            # print("laplace entropy MSE: {}\nlaplace KL entropy MSE: {}".format(MSE_uniform,MSE_KL))
+
+            if N == N_range[-1]:
+                iter = iter + 1
+                filename = misc.update_filename(path, filename, iter, rename=True)
+            util.io.save(
+                (N_range, H_unif_KL, H_unif_KSG, H_KL_laplace, H_KSG_laplace, iter),
+                os.path.join(path, filename),
             )
-            H_KL_laplace[i, ni] = kl(laplace_base)
-            H_KSG_laplace[i, ni] = ksg(laplace_base)
-
-        # H_unif_KL[i,ni],H_unif_KSG[i,ni] = ent.calc_entropy(sim_model = sim_laplace, n_samples = n_train_samples,base_samples=laplace_base,val_tol=0.01,method='both')
-
-        # MSE_uniform = np.mean((H_unif_KL[:i+1,N-1] - true_H_laplace)**2)
-        # MSE_KL = np.mean((H_KL_laplace[:i+1,N-1] - true_H_laplace)**2)
-        # print("laplace entropy MSE: {}\nlaplace KL entropy MSE: {}".format(MSE_uniform,MSE_KL))
-
-        if N == N_range[-1]:
-            iter = iter + 1
-            filename = misc.update_filename(path, filename, iter, rename=True)
-        util.io.save(
-            (N_range, H_unif_KL, H_unif_KSG, H_KL_laplace, H_KSG_laplace, iter),
-            os.path.join(path, filename),
-        )
